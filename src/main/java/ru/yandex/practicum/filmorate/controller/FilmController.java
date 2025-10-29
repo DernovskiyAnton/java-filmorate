49f5c3b36abbffa72d78ef494225905522fe3848
a.dernovskiy <dernovski@icloud.com>
2025-10-29 23:47:39 +0700

    Добавил контролеры Добавил модели

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,72 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
 
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
 @RestController
+@RequestMapping("/filmorate/films")
 public class FilmController {
+
+    private static final LocalDate MIN_RELEASE_DATE = LocalDate.of(1895, 12, 28);
+    private final Map<Long, Film> films = new HashMap<>();
+
+    @PostMapping
+    public Film add(@Valid @RequestBody Film film) {
+        log.info("Добавление нового фильма: {}", film.getName());
+        validReleaseDate(film);
+        film.setId(getNextId());
+        films.put(film.getId(), film);
+        log.debug("Фильм успешно добавлен");
+        return film;
+    }
+
+    @PutMapping
+    public Film update(@Valid @RequestBody Film newFilm) {
+        log.info("Обновление фильма: {}", newFilm.getName());
+        validReleaseDate(newFilm);
+
+        if (films.containsKey(newFilm.getId())) {
+            films.put(newFilm.getId(), newFilm);
+            log.debug("Фильм успешно обновлен");
+            return newFilm;
+        }
+        log.warn("Фильм с id = {} не найден", newFilm.getId());
+        throw new NotFoundException("Фильм с id = " + newFilm.getId() + " не найден.");
+    }
+
+    @GetMapping
+    public Collection<Film> findAll() {
+        return films.values();
+    }
+
+    private long getNextId() {
+        long currentMaxId = films.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+
+    private void validReleaseDate(Film film) {
+        if (film.getReleaseDate().isBefore(MIN_RELEASE_DATE)) {
+            log.warn("Ошибка в дате релиза по фильму с id = {}", film.getId());
+            throw new ValidationException("Дата релиза не может быть раньше 28 декабря 1895 года");
+        }
+    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,26 +1,24 @@
 package ru.yandex.practicum.filmorate.model;
 
 import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
 
-import java.time.Duration;
 import java.time.LocalDate;
 
 @Data
-public class Film {
+public class User {
 
     private Long id;
+    @NotBlank
+    @Email
+    private String email;
     @NotNull
     @NotBlank
+    private String login;
     private String name;
-    @Size(max = 200)
-    private String description;
     @NotNull
-    private LocalDate releaseDate;
-    @Positive
-    private Duration duration;
+    private LocalDate birthday;
 }
